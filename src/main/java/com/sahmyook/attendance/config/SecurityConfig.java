c774a40b988e9f7e67758919bf7ee8470eb02396
seungbin-park <tmdqls0212@naver.com>
2023-04-15 04:02:54 +0900

    로그인 인증 및 출퇴근 기능 구현

--- src/main/java/com/sahmyook/attendance/config/SecurityConfig.java
+++ src/main/java/com/sahmyook/attendance/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.sahmyook.attendance.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -8,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.access.expression.SecurityExpressionOperations.*;
 
 @Configuration
 @EnableWebSecurity
@@ -21,8 +21,9 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
         http.csrf().disable();
+        http.logout()
+                .logoutSuccessUrl("/login");
         http.authorizeRequests()
                 .requestMatchers("/main/**").authenticated()
                 .requestMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
@@ -34,6 +35,8 @@ public class SecurityConfig {
                 .passwordParameter("userPw")
                 .loginProcessingUrl("/loginProc")
                 .defaultSuccessUrl("/main");
+
+
 /*
                 .and()
                 .oauth2Login()
